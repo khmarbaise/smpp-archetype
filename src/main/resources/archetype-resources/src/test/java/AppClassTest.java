1efb5009225ea31ca0eaea4e358338036c8c9e0b
Karl Heinz Marbaise <kama@soebes.de>
2020-09-29 22:04:38 +0200

    Improved.

---
+++ src/main/resources/archetype-resources/src/test/java/AppClassTest.java
@@ -0,0 +1,13 @@
+package ${groupId};
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class AppClassTest {
+
+  @Test
+  void first_test() {
+    fail("Test not yet implemented.");
+  }
+}
